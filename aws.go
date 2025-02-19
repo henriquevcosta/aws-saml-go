0208ca4a8868071ef87cee3ce9b812443ab772d8
Henrique Costa <henrique@personal.local>
2025-02-19 15:15:50 +1100

    working scenario

---
+++ aws.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+	saml2 "github.com/russellhaering/gosaml2"
+)
+
+type AuthOptions struct {
+	SessionDuration int64
+	RoleARN         string
+	Region          string
+}
+
+type CredentialsOutputWriter interface {
+	WriteOutput(*sts.AssumeRoleWithSAMLOutput) error
+}
+
+// A credentialProcessResponse is the AWS credentials format that must be
+// returned when executing an external credential_process.
+// This type is copied from the aws go sdk
+type credentialProcessResponse struct {
+	// As of this writing, the Version key must be set to 1. This might
+	// increment over time as the structure evolves.
+	Version int
+
+	// The access key ID that identifies the temporary security credentials.
+	AccessKeyID string `json:"AccessKeyId"`
+
+	// The secret access key that can be used to sign requests.
+	SecretAccessKey string
+
+	// The token that users must pass to the service API to use the temporary credentials.
+	SessionToken string
+
+	// The date on which the current credentials expire.
+	Expiration *time.Time
+}
+
+type CredentialsProcessOutputWriter struct{}
+
+func (w *CredentialsProcessOutputWriter) WriteOutput(c *sts.AssumeRoleWithSAMLOutput) error {
+	forJSON := &credentialProcessResponse{
+		Version:         1,
+		AccessKeyID:     *c.Credentials.AccessKeyId,
+		SecretAccessKey: *c.Credentials.SecretAccessKey,
+		SessionToken:    *c.Credentials.SessionToken,
+		Expiration:      c.Credentials.Expiration,
+	}
+	jsonOutput, err := json.Marshal(forJSON)
+	if err != nil {
+		stderrlogger.Error("Could not marshall the credentials")
+		return err
+	}
+	stderrlogger.Debug("Encoded JSON to return", "payload", jsonOutput)
+	fmt.Printf("%s", jsonOutput)
+	return nil
+}
+
+type AWSAuthenticator struct {
+	AuthOptions *AuthOptions
+	Writer      CredentialsOutputWriter
+}
+
+type SAMLData struct {
+	EncodedSAMLResponse string
+	AssertionInfo       *saml2.AssertionInfo
+}
+
+func (auth *AWSAuthenticator) doAuth(samlData SAMLData) (*time.Time, error) {
+	sessionDuration := samlData.AssertionInfo.Values.Get("https://aws.amazon.com/SAML/Attributes/SessionDuration")
+	sessionDurationSeconds, err := strconv.ParseInt(sessionDuration, 10, 64)
+	if err != nil {
+		stderrlogger.Error("Could not parse session duration", "error", err)
+		return nil, err
+	}
+
+	roleDurationSecondsOpt := int64(auth.AuthOptions.SessionDuration)
+	if roleDurationSecondsOpt == 0 {
+		roleDurationSecondsOpt = int64(3600) // 1 hour default
+	}
+	if roleDurationSecondsOpt < sessionDurationSeconds {
+		sessionDurationSeconds = roleDurationSecondsOpt
+	}
+
+	principalARN := ""
+	if roles, foundRoles := samlData.AssertionInfo.Values["https://aws.amazon.com/SAML/Attributes/Role"]; foundRoles {
+		found := false
+		for _, val := range roles.Values {
+			stderrlogger.Debug("", "role attribute", val)
+			split := strings.Split(val.Value, ",")
+			if split[0] == auth.AuthOptions.RoleARN {
+				principalARN = split[1]
+				found = true
+				break
+			}
+		}
+		if !found {
+			stderrlogger.Error("SAML response didn't contain the requested role")
+			return nil, errors.New("SAML response didn't contain the requested role")
+		}
+	} else {
+		stderrlogger.Error("SAML response didn't contain the roles assertion")
+		return nil, errors.New("SAML response didn't contain the roles assertion")
+	}
+
+	session := session.Must(session.NewSession())
+
+	stsInput := &sts.AssumeRoleWithSAMLInput{
+		RoleArn:         &auth.AuthOptions.RoleARN,
+		PrincipalArn:    &principalARN,
+		SAMLAssertion:   &samlData.EncodedSAMLResponse,
+		DurationSeconds: &sessionDurationSeconds,
+	}
+	stsClient := sts.New(session, aws.NewConfig().WithRegion(auth.AuthOptions.Region))
+
+	output, err := stsClient.AssumeRoleWithSAML(stsInput)
+	if err != nil {
+		// return nil, err
+		stderrlogger.Error("Could not assume role", "error", err)
+		panic(err)
+	}
+	stderrlogger.Debug("Session obtained", "output", output)
+
+	err = auth.Writer.WriteOutput(output)
+	if err != nil {
+		return nil, err
+	}
+
+	return output.Credentials.Expiration, nil
+}
---
+++ main.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"log/slog"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
+
+	_ "embed"
+
+	saml2 "github.com/russellhaering/gosaml2"
+	"gopkg.in/ini.v1"
+)
+
+//go:embed files/authed.html
+var htmlSuccess string
+
+var stderrlogger *slog.Logger
+
+type profileConfigs struct {
+	idpCallURI string
+	roleARN    string
+	awsRegion  string
+}
+
+func readConfig(profile string) (profileConfigs, error) {
+
+	// Load the INI file and produce configs
+	usr, _ := user.Current()
+	homeDir := usr.HomeDir
+	configFilePath := filepath.Join(homeDir, ".aws/config") // TODO We might want to handle this more dynamically
+	cfg, err := ini.Load(configFilePath)
+	if err != nil {
+		stderrlogger.Error("Fail to read AWS config file", "error", err)
+	}
+	configs := profileConfigs{
+		roleARN:    "some-role",
+		awsRegion:  "ap-southeast-2",
+		idpCallURI: "https://accounts.google.com/o/saml2/initsso?",
+	}
+
+	// profileName := "qlmgt"
+	// // Get the values from the desired section (e.g., "default")
+	// defaultSection := cfg.Section("default")
+	// baseURL := defaultSection.Key("base_url").String()
+	// clientID := defaultSection.Key("client_id").String()
+	// tenantID := defaultSection.Key("tenant_id").String()
+	_ = cfg
+
+	return configs, nil
+}
+
+func main() {
+	port := *flag.Int("port", 35002, "Port that you have configured in your IDP for the callback (default 35002)")
+	hostAndPort := fmt.Sprintf("127.0.0.1:%d", port)
+
+	debugMode := *flag.Bool("debug", false, "Enable verbose logging - WILL INCLUDE CREDENTIALS IN LOG!")
+	var loggerOpts *slog.HandlerOptions
+	if debugMode {
+		loggerOpts = &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}
+	} else {
+		loggerOpts = &slog.HandlerOptions{}
+	}
+	stderrlogger = slog.New(slog.NewTextHandler(os.Stderr, loggerOpts))
+
+	profileName := flag.Arg(0)
+
+	configs, err := readConfig(profileName)
+	if err != nil {
+		stderrlogger.Error("Could not obtain profile information", "profile", profileName, "error", err)
+		panic(err)
+	}
+
+	authOptions := &AuthOptions{
+		RoleARN: configs.roleARN,
+		Region:  configs.awsRegion,
+	}
+
+	serviceProvider := &saml2.SAMLServiceProvider{
+		SkipSignatureValidation:     true,
+		AssertionConsumerServiceURL: fmt.Sprintf("http://%s", hostAndPort),
+		AudienceURI:                 "urn:amazon:webservices:cli",
+	}
+
+	mux := http.NewServeMux()
+	srv := http.Server{
+		Handler: mux,
+	}
+
+	writer := &CredentialsProcessOutputWriter{}
+	authenticator := &AWSAuthenticator{
+		AuthOptions: authOptions,
+		Writer:      writer,
+	}
+
+	handler := &SAMLResponseHandler{
+		server:          &srv,
+		serviceProvider: serviceProvider,
+		authenticator:   authenticator,
+		profileName:     profileName,
+	}
+	// Start the HTTP server
+	mux.HandleFunc("/", handler.handleSAMLResponse)
+	stderrlogger.Info("Starting server", "port", port)
+	l, err := net.Listen("tcp", hostAndPort)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stderrlogger.Info("Opening URL", "url", configs.idpCallURI)
+	openbrowser(configs.idpCallURI)
+
+	if err := srv.Serve(l); err != http.ErrServerClosed {
+		stderrlogger.Error("Error running server", "error", err)
+		panic(err)
+	}
+}
+
+type SAMLResponseHandler struct {
+	server          *http.Server
+	serviceProvider *saml2.SAMLServiceProvider
+	authenticator   *AWSAuthenticator
+	profileName     string
+}
+
+func openbrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (h *SAMLResponseHandler) handleSAMLResponse(rw http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		// Process the POST request here
+		stderrlogger.Debug("Received a POST request")
+		err := req.ParseForm()
+		if err != nil {
+			stderrlogger.Error("Could not parse POST form", "error", err)
+			rw.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		encodedSAMLResponse := req.FormValue("SAMLResponse")
+		assertionInfo, err := h.serviceProvider.RetrieveAssertionInfo(encodedSAMLResponse)
+		stderrlogger.Debug("SAML Response received", "SAML-response", encodedSAMLResponse)
+		if err != nil {
+			stderrlogger.Error("Could not parse SAML Assertion", "error", err)
+			rw.WriteHeader(http.StatusForbidden)
+			return
+		}
+		if assertionInfo.WarningInfo.InvalidTime {
+			stderrlogger.Error("Invalid SAML assertion time")
+			fmt.Println("invalid time")
+			rw.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		if assertionInfo.WarningInfo.NotInAudience {
+			stderrlogger.Error("invalid audience")
+			rw.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		stderrlogger.Debug("NameID", "NameID", assertionInfo.NameID)
+		stderrlogger.Debug("Assertions:")
+		for key, val := range assertionInfo.Values {
+			stderrlogger.Debug("", "key", key, "val", val)
+		}
+		stderrlogger.Debug("Warnings:")
+		stderrlogger.Debug("", "warningInfo", assertionInfo.WarningInfo)
+
+		samlData := SAMLData{
+			EncodedSAMLResponse: encodedSAMLResponse,
+			AssertionInfo:       assertionInfo,
+		}
+
+		expiration, err := h.authenticator.doAuth(samlData)
+		if err != nil {
+			http.Error(rw, fmt.Sprintf("Error processing authentication: %s", err), http.StatusInternalServerError)
+			return
+		}
+
+		var htmlComposed string
+		htmlComposed = strings.ReplaceAll(htmlSuccess, "__REPLACED_DATE_HERE__", expiration.Format(time.RFC3339))
+		htmlComposed = strings.ReplaceAll(htmlComposed, "__REPLACED_PROFILE_NAME_HERE__", h.profileName)
+		rw.Header().Set("Content-type", "text/html")
+		rw.Write([]byte(htmlComposed))
+
+	} else {
+		http.Error(rw, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+	go h.server.Shutdown(context.Background())
+}
